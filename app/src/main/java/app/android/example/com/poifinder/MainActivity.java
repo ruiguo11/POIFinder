b89c89922989495329885ffd8de9475e44927d04
Rui <ruiguo80@icloud.com>
2016-10-30 12:13:04 +1300

    Initial Commit get Last known Location

--- app/src/main/java/app/android/example/com/poifinder/MainActivity.java
+++ app/src/main/java/app/android/example/com/poifinder/MainActivity.java
@@ -138,6 +138,22 @@ public class MainActivity extends AppCompatActivity implements
         }
 
 
+        mLocationRequest = LocationRequest.create();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setInterval(1000);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+
+        //textView.setText(mLastLocation.toString());
 
 
 
